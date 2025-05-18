825aa5199bb3bfe9a1633fe5dd443937c24e6098
the-gigi <the.gigi@gmail.com>
2025-05-17 21:18:43 -0700

    cleanup

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-from pprint import pprint as pp
 import os
-from typing import List, Dict
-
 import sh
 import json
-
 from openai import OpenAI
 
-k = sh.kubectl
 client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 model_name = "gpt-4o"
 
@@ -35,7 +30,7 @@ tools = [
 ]
 
 
-def send(messages: List[Dict[str, any]]) -> str:
+def send(messages: list[dict[str, any]]) -> str:
     response = client.chat.completions.create(
         model=model_name, messages=messages, tools=tools, tool_choice="auto")
     r = response.choices[0].message
@@ -46,46 +41,31 @@ def send(messages: List[Dict[str, any]]) -> str:
             tool_calls=[dict(
                 id=t.id,
                 type=t.type,
-                function=dict(
-                    name=t.function.name,
-                    arguments=t.function.arguments
-                )) for t in r.tool_calls if t.function])
+                function=dict(name=t.function.name,arguments=t.function.arguments)
+            ) for t in r.tool_calls if t.function])
         messages.append(message)
         for t in r.tool_calls:
             if t.function.name == 'kubectl':
                 cmd = json.loads(t.function.arguments)['cmd'].split()
-                result = k(cmd)
-                messages.append(
-                    {
-                        "tool_call_id": t.id,
-                        "role": "tool",
-                        "name": t.function.name,
-                        "content": result,
-                    }
-                )
+                result = sh.kubectl(cmd)
+                messages.append(dict(tool_call_id=t.id, role="tool", name=t.function.name, content=result))
         return send(messages)
     return r.content.strip()
 
-
 def main():
-    print("Interactive Kubernetes Chat")
-    print("Type 'exit' to quit.")
+    print("Interactive Kubernetes Chat. Type 'exit' to quit.")
     messages = [{'role': 'system', 'content': 'You are a Kubernetes expert ready to help'}]
-
     while True:
         user_input = input("You: ")
         if user_input.lower() == 'exit':
             print("Goodbye!")
             break
-
         messages.append({
             "role": "user",
             "content": user_input,
         })
-
         response = send(messages)
-        print(f"AI: {response}")
-        print('----------')
+        print(f"AI: {response}\n----------")
 
 
 if __name__ == "__main__":
